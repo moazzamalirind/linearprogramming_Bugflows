30a9530411218b3a45d582e6b8b1511386436013
MOAZZAM ALI RIND <moazzamalirind@gmail.com>
2020-02-25 12:29:10 -0700

    Not required bcz outdated stuff

--- 225a/gamsnext.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-gmsba_nt.cmd "C:\Rosenberg\Work\USU\Students\RindMoazzam\Repositories\-Bug-flow-\225a\" "C:\Rosenberg\Work\USU\Students\RindMoazzam\Repositories\-Bug-flow-\" %1 "C:\Rosenberg\Work\USU\Students\RindMoazzam\Repositories\-Bug-flow-\225a\gamscntr.dat" "C:\GAMS\win64\24.2\" "BARON"
